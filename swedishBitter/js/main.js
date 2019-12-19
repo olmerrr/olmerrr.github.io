4c74ce5c7ad317d462c27b20e713a7f2a520b468
Yaroslav <dombrovskiy91@gmail.com>
2019-12-19 11:48:59 +0200

    last
    last

--- swedishBitter/js/main.js
+++ swedishBitter/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  $('.list__inner .tab').on('click', function(event) {
+  $('.list__inner .tab').on('click' ,function(event) {
     var id = $(this).attr('data-id');
       $('.list__inner').find('.tab-item').removeClass('active-tab').hide();
       $('.list__inner .tabs').find('.tab').removeClass('active');
