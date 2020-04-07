9a7fa134b154e0ede157a2b2835dc73d931addf0
Yaroslav <dombrovskiy91@gmail.com>
2020-04-07 16:07:32 +0300

    desctop

--- w-Axis/assets/js/main.js
+++ w-Axis/assets/js/main.js
@@ -1,34 +1,31 @@
 $(function(){
 
-  $('.slider__inner,.news__slider-inner').slick({
-    nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
-    prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
-    infinite:false,
+  $('.slider__inner').slick({
+    responsive: [
+      {
+          breakpoint: 2048,
+          settings: "unslick"
+      },
+      {
+         breakpoint: 600,
+         settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+      }
+  },
+]
   });
 $('select').styler();
-$('.header__btn-menu').on('click', function(){
-  $('.menu ul').slideToggle();
-});
-jQuery( document ).ready(function() {
-  jQuery('#scrollup').mouseover( function(){
-    jQuery( this ).animate({opacity: 0.65},100);
-  }).mouseout( function(){
-    jQuery( this ).animate({opacity: 1},100);
-  }).click( function(){
-    //window.scroll(0 ,0);  - резкая прокрутка
-        jQuery('body,html').animate({scrollTop:0},800);
-    return false;
-  });
-
-  jQuery(window).scroll(function(){
-    if ( jQuery(document).scrollTop() > 0 ) {
-      jQuery('#scrollup').fadeIn('fast');
-    } else {
-      jQuery('#scrollup').fadeOut('fast');
+window.addEventListener("resize", function() {
+  if (window.innerWidth <= 768) {
+    $('.slider__inner').slick('unslick');
+    sliderIsLive = false;
+  }
+  else {
+    if (sliderIsLive) {
+      $('.slider__inner').slick();
+      sliderIsLive = true;
     }
-  });
+  }
 });
-
-
 });
-
