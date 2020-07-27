8acd45b2af7dd96d45a3297b1d197790da51b3a5
Yaroslav <dombrovskiy91@gmail.com>
2020-07-27 22:26:33 +0300

    desctop + tablet + mob
    desctop + tablet + mob

--- webteam/assets/styles/js/main.js
+++ webteam/assets/styles/js/main.js
@@ -4,6 +4,5 @@ $(function(){
     nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
     prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
     infinite:true,
-
   });
 });
