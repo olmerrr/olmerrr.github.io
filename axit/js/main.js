8795a2ec6983d4a18f2544bc741ed2c9ab329445
Yaroslav Dombrovskyi <dombrovskiy91@gmail.com>
2026-01-15 13:10:51 +0100

    Update main.js change logic for footer
    change logic for footer  also added commeents

--- axit/js/main.js
+++ axit/js/main.js
@@ -1,18 +1,26 @@
+// Burger menu
 $(function(){
   $('.menu__btn').on('click', function(){
     $('.menu__list').slideToggle();
   });
 });
 
+// Up button
 $(function() {
   $(window).scroll(function() {
-  if($(this).scrollTop() != 0) {
-  $('#toTop').fadeIn();
-  } else {
-  $('#toTop').fadeOut();
-  }
+    if($(this).scrollTop() != 0) {
+      $('#toTop').fadeIn();
+    } else {
+      $('#toTop').fadeOut();
+    }
   });
   $('#toTop').click(function() {
-  $('body,html').animate({scrollTop:0},800);
+    $('body,html').animate({scrollTop:0},800);
   });
-  });
+});
+
+// Footer current year
+$(function() {
+  const currentYear = new Date().getFullYear();
+  $('#current-year').text(currentYear); // Вставляем в span с id="current-year"
+});
